7a5024f8df7843f48740a0b3855f73cf808751da
Z1yim <marcowong0829@gmail.com>
2023-01-13 16:41:18 +1000

    finished footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,28 +1,39 @@
 import React from "react";
+import {
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 import "../styles/Footer.scss";
 
 export default function Footer() {
   return (
     <footer>
-      <h2>randy.tct</h2>
-      <ul>
-        <li>Randall Cheng</li>
+      <h2>randy.ctc</h2>
+      <ul id="details">
+        <li id="name">Randall Cheng</li>
         <li>Phone: 04 2460 4395</li>
         <li>rcrc1026@gmail.com</li>
       </ul>
-      <ul>
+      <ul id="socials">
         <li>
-          <a href="/">instagram</a>
+          <a href="/">
+            <AiFillInstagram />
+          </a>
         </li>
         <li>
-          <a href="/">facebook</a>
+          <a href="/">
+            <AiFillFacebook />
+          </a>
         </li>
         <li>
-          <a href="/">linkedin </a>
+          <a href="/">
+            <AiFillLinkedin />
+          </a>
         </li>
-        <p>(copyright icon)Randy.CTC. All rights reserved.</p>
       </ul>
+      <p>&copy; Randy.CTC. All rights reserved.</p>
     </footer>
   );
 }
--- app/WorksContainer.tsx
+++ app/WorksContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/WorksContainer.scss";
 
 interface Props {
   title: string;
@@ -7,13 +8,13 @@ interface Props {
 
 export default function WorksContainer({ title, main }: Props) {
   return (
-    <div>
+    <main className="works-container">
       <h2>{title}</h2>
       {main}
       <p>
         Shot with Nikon D5500, AF-P NIKKOR 70-300mm f/4.5-5.6E ED VR & Nikon Z5,
         NIKKOR Z 85mm f/1.8 S
       </p>
-    </div>
+    </main>
   );
 }
